926bc3d13a7edf99cf4ca22be6a1d84b129dd676
MyCashBar <32549596+MyCashBar@users.noreply.github.com>
2017-10-15 12:41:38 +0300

    1

--- stat.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ip = getenv ('REMOTE_ADDR');
-$date=date("j F, Y, g:i a");
-$name = $_POST['id'];
-$fp = fopen('stat.txt', 'a');
-fwrite($fp,'Kod: ' .$name.  ' Date and Time:' .$date. ' IP: ' .$ip. "\n");
-fclose($fp);
-
-?>
